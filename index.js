878bd28080ca111a993eec3915f029ac46c9c5e8
Kieren Holt <kierenholt@gmail.com>
2022-11-14 11:18:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 
-//TEST
+var AWS = require("aws-sdk");
 
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+
+exports.handler = function(event, context) {
+    var eventText = JSON.stringify(event, null, 2);
+    console.log("Received event:", eventText);
+    
+    var sns = new AWS.SNS();
+    var bodyObj = JSON.parse(event.Records[0].body);
+    var params = {
+        Message: bodyObj.Message, 
+        Subject: "Test SNS From Lambda",
+        PhoneNumber: bodyObj.Subject,
+        //PhoneNumber: "+447986670340"
+        //TopicArn: "arn:aws:sns:eu-west-2:401739403606:KierensPhone"
     };
-    return response;
+    sns.publish(params, context.done).promise()
+    .then((response) => console.log("success with response: "))
+    .catch((err) => console.error("failure with error: "));
 };
